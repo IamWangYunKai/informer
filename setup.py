eae7a6e009cb90b20eadbad2baa97aab3c717501
IamWangYunKai <615349118@163.com>
2020-02-18 22:07:42 +0800

    update test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='informer',
-    version='0.0.1',
+    version='0.0.2',
     author='Wang Yunkai',
     author_email='wangyunkai.zju@gmail.com',
     url='https://github.com/IamWangYunKai/informer',
